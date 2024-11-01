da80970ac517d1fd7ffe9063713f20f1526d9b72
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-11-01 15:17:51 +0100

    remove bench script

--- bench.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for mode in Debug ReleaseSmall ReleaseSafe ReleaseFast; do
-    zig build -Doptimize=$mode & >/dev/null
-done
-wait
-for mode in Debug ReleaseSmall ReleaseSafe ReleaseFast; do
-    zig build -Doptimize=$mode run -- $1
-done
