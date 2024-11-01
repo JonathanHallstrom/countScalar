5a00bbecccb441bf5df9ce35ccd4d944c6456635
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-11-01 14:35:29 +0100

    remove my implementation, only keep prottys, also added constraint to make it work more types

--- bench.sh
+++ bench.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 for mode in Debug ReleaseSmall ReleaseSafe ReleaseFast; do
-    ~/dev/zig/zig/build/stage3/bin/zig build -Dcpu=znver5 -Doptimize=$mode & >/dev/null
+    zig build -Doptimize=$mode & >/dev/null
 done
 wait
 for mode in Debug ReleaseSmall ReleaseSafe ReleaseFast; do
-    ~/dev/zig/zig/build/stage3/bin/zig build -Dcpu=znver5 -Doptimize=$mode run -- $1
+    zig build -Doptimize=$mode run -- $1
 done
